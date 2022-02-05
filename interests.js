8019c65da055a27b14b368c414ab6f509f43acc1
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2022-02-05 00:48:08 -0500

    complete changing model to commands

--- interests.js
+++ interests.js
@@ -1,7 +1,9 @@
 const interests = {
 	sports: ["basketball", "soccer", "racing", "baseball"],
+	TV: ["sitcoms", "action", "teen drama", "comedy", "mystery", "thriller", "romance"],
 	music: ["rock", "pop", "hip-hop", "pop", "indie"],
-	videogames: ["shooting", "MOBA", "racing", "FPS"],
+	videogames: ["shooting", "MOBA", "racing", "FPS", "simulation"],
+	miscellaneous: ["cooking", "playing music", "writing", "travelling"]
 };
 
 module.exports = interests;
